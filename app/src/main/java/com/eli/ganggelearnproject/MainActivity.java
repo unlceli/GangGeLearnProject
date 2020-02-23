21b9986c776a1acc2b9eb87ea3da2babbc20c3eb
lijj <125956902@qq.com>
2020-02-23 18:07:17 +0800

    刚哥初始化。

---
+++ app/src/main/java/com/eli/ganggelearnproject/MainActivity.java
@@ -0,0 +1,26 @@
+package com.eli.ganggelearnproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
